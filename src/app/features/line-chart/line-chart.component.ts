1581e5142932a83f831ad882688eb1844f7f82f0
Franco Vegini <francovegini@hotmail.com>
2019-08-18 23:56:54 -0300

    ajuste

--- src/app/features/line-chart/line-chart.component.ts
+++ src/app/features/line-chart/line-chart.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 
 @Component({

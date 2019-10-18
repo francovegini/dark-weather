6800b2a4bb5d8f097eb68ee5d8b78de3e251aaeb
Franco Vegini <francovegini@hotmail.com>
2019-10-18 00:50:39 -0300

    bugfix

--- src/app/features/line-chart/line-chart.component.ts
+++ src/app/features/line-chart/line-chart.component.ts
@@ -1,58 +1,64 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 
 @Component({
     selector: 'app-line-chart',
     templateUrl: './line-chart.component.html'
 })
-export class LineChartComponent {
+export class LineChartComponent implements OnInit {
 
     @Input() categories: any[];
     @Input() data: any[];
 
-    chart = new Chart(<any>{
-        xAxis: {
-            categories: this.categories,
-            plotLines: [{
-                color: '#FF0000',
-                width: 2,
-                value: 5.5,
-                id: 'plotline-1'
-            }],
-        },
-        yAxis: {
-            plotLines: [{
-                color: 'green',
-                width: 2,
-                value: 155,
-                dashStyle: 'shortdash',
-            }],
-        },
-
-        title: {
-            text: 'Temperatura da semana'
-        },
-
-        credits: {
-            enabled: false
-        },
-
-        series: [{
-            data: this.data
-        }],
-
-        annotations: [{
-            labels: [{
-                point: 'max',
-                text: 'Max'
-            }, {
-                point: 'min',
-                text: 'Min',
-                backgroundColor: 'white'
-            }]
-        }]
-    });
+    public chart: any;
 
     constructor() {
+    }
+
+    ngOnInit(): void {
+        if (this.categories && this.data) {
+            this.chart = new Chart(<any>{
+                xAxis: {
+                    categories: this.categories,
+                    plotLines: [{
+                        color: '#FF0000',
+                        width: 2,
+                        value: 5.5,
+                        id: 'plotline-1'
+                    }],
+                },
+                yAxis: {
+                    plotLines: [{
+                        color: 'green',
+                        width: 2,
+                        value: 155,
+                        dashStyle: 'shortdash',
+                    }],
+                },
+
+                title: {
+                    text: 'Temperatura da semana'
+                },
+
+                credits: {
+                    enabled: false
+                },
+
+                series: [{
+                    data: this.data
+                }],
+
+                annotations: [{
+                    labels: [{
+                        point: 'max',
+                        text: 'Max'
+                    }, {
+                        point: 'min',
+                        text: 'Min',
+                        backgroundColor: 'white'
+                    }]
+                }]
+            });
+        }
     }
 }

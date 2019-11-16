0d4360da569d2288ee1e83eb48960d82a98bdb3c
Franco Vegini <francovegini@hotmail.com>
2019-11-15 23:02:08 -0200

    ajuste

--- src/app/features/navbar/navbar.module.ts
+++ src/app/features/navbar/navbar.module.ts
@@ -2,9 +2,7 @@ import { NgModule } from '@angular/core';
 import { NavbarComponent } from './navbar.component';
 
 @NgModule({
-    declarations: [
-        NavbarComponent
-    ],
+    declarations: [NavbarComponent],
     exports: [NavbarComponent]
 })
 export class NavbarModule {

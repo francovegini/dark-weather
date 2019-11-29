13a6571d15a18b3b22ef86d8c67649eabc435808
Franco Vegini <francovegini@hotmail.com>
2019-11-28 23:58:51 -0200

    ajustes doc

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -14,24 +14,26 @@ export class ApiService {
 
     /**
      * Obtem a previsão dos próximos 15 dias de uma determinada cidade a partir do seu respectivo ID.
+     *
      * @param id ID da cidade
      */
     public getWeatherNext15Days(id: string) {
-        return this.httpClient.get(`${this.API_URL}forecast/locale/${id}/days/15?token=${this.TOKEN}`);
+        return this.httpClient.get(`${ this.API_URL }forecast/locale/${ id }/days/15?token=${ this.TOKEN }`);
     }
 
     /**
      * Obtem o ID de uma ou mais cidade a partir do nome e/ou estado.
+     *
      * @param name Nome da cidade
      * @param state Estado
      */
     public getIdByNameOrState(name?: string, state?: string) {
         if ((name !== '' || name) && (state !== '' || state)) {
-            return this.httpClient.get(`${this.API_URL}locale/city?name=${name}&state=${state}&token=${this.TOKEN}`);
+            return this.httpClient.get(`${ this.API_URL }locale/city?name=${ name }&state=${ state }&token=${ this.TOKEN }`);
         } else if ((name !== '' || name) && (!state || state === '')) {
-            return this.httpClient.get(`${this.API_URL}locale/city?name=${name}&token=${this.TOKEN}`);
+            return this.httpClient.get(`${ this.API_URL }locale/city?name=${ name }&token=${ this.TOKEN }`);
         } else if ((!name || name === '') && (state !== '' || state)) {
-            return this.httpClient.get(`${this.API_URL}locale/city?state=${state}&token=${this.TOKEN}`);
+            return this.httpClient.get(`${ this.API_URL }locale/city?state=${ state }&token=${ this.TOKEN }`);
         }
     }
 }
